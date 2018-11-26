e07c48b3d93810036ecfc90991bc40a1b36bed68
David Falana <falanadavid@gmail.com>
2018-11-26 14:42:33 -0500

    Draft #2

--- src/Database.java
+++ src/Database.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 /**
  * This class will serve as the intermediary between our ATM program and
  * the database of BankAccounts. It'll be responsible for fetching accounts
@@ -6,5 +11,22 @@
  */
 
 public class Database {
+	public static void main(String[] args) throws Exception {
 	
+		try (BufferedReader br = new BufferedReader (new FileReader("accounts-db.txt")))
+	{
+		String line;
+		while ((line = br.readLine()) != null) {
+			
+		}
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		
+	}
 }
+
